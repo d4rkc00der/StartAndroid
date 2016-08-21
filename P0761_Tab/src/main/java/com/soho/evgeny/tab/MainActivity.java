278468d2eac1d119c8868c2302d2cdaea6a4c208
evgeny <shumakov.e@gmail.com>
2016-08-21 14:58:30 +0300

    PrefActivity.java Files

---
+++ P0761_Tab/src/main/java/com/soho/evgeny/tab/MainActivity.java
@@ -0,0 +1,47 @@
+package com.soho.evgeny.tab;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TabHost;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TabHost tabHost = (TabHost)findViewById(android.R.id.tabhost);
+
+        tabHost.setup();
+        TabHost.TabSpec tabSpec;
+
+
+        tabSpec = tabHost.newTabSpec("tag1");
+        tabSpec.setIndicator("Вкладка 1");
+        tabSpec.setContent(R.id.tvTab1);
+        tabHost.addTab(tabSpec);
+
+        tabSpec = tabHost.newTabSpec("tag2");
+        tabSpec.setIndicator("Вкладка2",getResources().getDrawable(R.drawable.tab_icon_selector));
+        tabSpec.setContent(R.id.tvTab2);
+        tabHost.addTab(tabSpec);
+
+        tabSpec = tabHost.newTabSpec("tag3");
+        View v = getLayoutInflater().inflate(R.layout.tabheader,null);
+        tabSpec.setIndicator(v);
+        tabSpec.setContent(R.id.tvTab3);
+        tabHost.addTab(tabSpec);
+
+        tabHost.setCurrentTabByTag("tag2");
+
+        tabHost.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
+            @Override
+            public void onTabChanged(String s) {
+                Toast.makeText(getBaseContext(),"tabId="+s,Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
