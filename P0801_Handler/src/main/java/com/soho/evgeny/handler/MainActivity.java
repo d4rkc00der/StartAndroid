7feaffb4f301dece06d3048065bfa0295b4ba22a
Evgeny Shumakov <evgeny@async.local>
2016-08-23 15:34:45 +0300

    Handler implemented. MultiThread support implemented

--- P0801_Handler/src/main/java/com/soho/evgeny/handler/MainActivity.java
+++ P0801_Handler/src/main/java/com/soho/evgeny/handler/MainActivity.java
@@ -1,5 +1,6 @@
 package com.soho.evgeny.handler;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
@@ -19,23 +20,40 @@ public class MainActivity extends AppCompatActivity {
     Button btnStart;
     Button btnTest;
 
+    @SuppressLint("HandlerLeak")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tvInfo = (TextView)findViewById(R.id.tvInfo);
+        btnStart = (Button)findViewById(R.id.btnStart);
+        // Основной поток
+        myHandler = new Handler(){
+            public void handleMessage(android.os.Message message){
+                tvInfo.setText("Закачано файлов:"+message.what);
+                if(message.what==10) btnStart.setEnabled(true);
+            };
+        };
     }
 
     void onclick(View v) {
         switch (v.getId()) {
             case R.id.btnStart:
-                for (int i = 0; i < 10; i++) {
-                    downloadFile();
-                    tvInfo.setText("Закачано файлов:"+i);
-
-                    Log.d(LOG_TAG,"Закачано файлов:"+i);
+                btnStart.setEnabled(false);
+                // Отдельный поток
+                Thread t = new Thread(new Runnable() {
+                public void run() {
+                    for (int i = 1; i <= 10; i++) {
+                        // долгий процесс
+                        downloadFile();
+                        // отправляем в основной поток данные
+                        myHandler.sendEmptyMessage(i);
+                        Log.d(LOG_TAG, "i = " + i);
+                    }
                 }
-                break;
+            });
+            t.start();
+            break;
             case R.id.btnTest:
                 Log.d(LOG_TAG,"Test");
                 break;
@@ -46,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
 
     void downloadFile(){
         try {
-            TimeUnit.SECONDS.sleep(5);
+            TimeUnit.SECONDS.sleep(1);
         }
         catch (InterruptedException e) {
             Log.d(LOG_TAG,e.getMessage());
