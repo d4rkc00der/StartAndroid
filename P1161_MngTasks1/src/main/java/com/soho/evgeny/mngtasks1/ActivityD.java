33da2fd8d6613b40329e95bc40902c7618176314
Evgeny Shumakov <evgeny@async.local>
2016-09-27 18:50:23 +0300

    Task management

---
+++ P1161_MngTasks1/src/main/java/com/soho/evgeny/mngtasks1/ActivityD.java
@@ -0,0 +1,14 @@
+package com.soho.evgeny.mngtasks1;
+
+import android.content.Intent;
+import android.view.View;
+
+/**
+ * Created by evgeny on 27.09.16.
+ */
+public class ActivityD extends MainActivity{
+    @Override
+    public void onClick(View v) {
+        startActivity(new Intent(this, ActivityD.class));
+    }
+}
---
+++ P1162_MngTasks2/src/main/java/com/soho/evgeny/mngtasks2/MainActivity.java
@@ -0,0 +1,42 @@
+package com.soho.evgeny.mngtasks2;
+
+import android.app.ActivityManager;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    final String LOG_TAG = "MyLog";
+    List<ActivityManager.RunningTaskInfo> list;
+    ActivityManager am;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle(getResources().getString(R.string.app_name) + " : " + getLocalClassName());
+        am = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
+    }
+
+    public void onClick(View v) {
+        startActivity(new Intent("mngtasks1_activity_c"));
+    }
+
+
+    public void onInfoClick(View v) {
+        list = am.getRunningTasks(10);
+        for (ActivityManager.RunningTaskInfo task : list) {
+            if (task.baseActivity.flattenToShortString().startsWith("com.soho.evgeny.mngtasks")) {
+                Log.d(LOG_TAG, "------------------");
+                Log.d(LOG_TAG, "Count: " + task.numActivities);
+                Log.d(LOG_TAG, "Root: " + task.baseActivity.flattenToShortString());
+                Log.d(LOG_TAG, "Top: " + task.topActivity.flattenToShortString());
+            }
+        }
+    }
+}
